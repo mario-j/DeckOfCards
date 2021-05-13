45834d08326589e5f345dd8f4c799a39b58c0ca0
Mario Jordan <mjordanbartolo@gmail.com>
2021-05-12 22:53:56 -0500

    Add suit and rank enums

---
+++ DeckOfCards/Enums/SuitEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeckOfCards.Enums
+{
+	public enum SuitEnum
+	{
+		Spades,
+		Clubs,
+		Diamonds,
+		Hearts
+	}
+}
